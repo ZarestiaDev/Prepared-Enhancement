3a502dc7f2c6a03fb08b11ea08f6ca37dd11f6d1
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2023-03-04 13:01:41 +0100

    Fix handler updating of prepared field

--- campaign/scripts/prepared_enhancement.lua
+++ campaign/scripts/prepared_enhancement.lua
@@ -1,28 +1,36 @@
 function onInit()
-    if super and super.onInit then
-        super.onInit();
-    end
-
-    local node = DB.getChild(getDatabaseNode(), "...");
-    DB.addHandler(DB.getPath(node, "level"), "onUpdate", cyclerChanged);
-	DB.addHandler(DB.getPath(node, "abilities.*.score"), "onUpdate", cyclerChanged);
+	if super and super.onInit then
+		super.onInit();
+	end
+	
+	local node = DB.getChild(getDatabaseNode(), "...");
+	DB.addHandler(DB.getPath(node, "level"), "onUpdate", onHandlerUpdate);
+	DB.addHandler(DB.getPath(node, "abilities.*.score"), "onUpdate", onHandlerUpdate);
 end
 
 function onClose()
-    local node = DB.getChild(getDatabaseNode(), "...");
-    DB.removeHandler(DB.getPath(node, "level"), "onUpdate", cyclerChanged);
-	DB.removeHandler(DB.getPath(node, "abilities.*.score"), "onUpdate", cyclerChanged);
+	local node = DB.getChild(getDatabaseNode(), "...");
+	DB.removeHandler(DB.getPath(node, "level"), "onUpdate", onHandlerUpdate);
+	DB.removeHandler(DB.getPath(node, "abilities.*.score"), "onUpdate", onHandlerUpdate);
+end
+
+function onHandlerUpdate()
+	if DB.getValue(getDatabaseNode(), "preparedmod", "") == "" then
+		return;
+	end
+	
+	cyclerChanged();
 end
 
 function cyclerChanged()
-    local nodeChar = getDatabaseNode();
-    local rActor = DB.getChild(nodeChar, "...");
-    local sAbility = DB.getValue(nodeChar, "preparedmod", "");
-    local nAbilityBonus = ActorManager5E.getAbilityBonus(rActor, sAbility);
-
-    prepared.setValue(nAbilityBonus);
+	local nodeChar = getDatabaseNode();
+	local rActor = DB.getChild(nodeChar, "...");
+	local sAbility = DB.getValue(nodeChar, "preparedmod", "");
+	local nAbilityBonus = ActorManager5E.getAbilityBonus(rActor, sAbility);
+	
+	prepared.setValue(nAbilityBonus);
 end
 
 function cyclerVisible(bPrep)
-    preparedmod.setVisible(bPrep);
+	preparedmod.setVisible(bPrep);
 end
